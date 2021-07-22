ab74ce4ca452ac74dcb3b2b67a123819e6b4faf9
Denis Fäcke <serayaeryn@gmail.com>
2021-07-22 18:25:51 +0200

    fix tests

--- testdata/TestAddressProvider.js
+++ testdata/TestAddressProvider.js
@@ -3,16 +3,16 @@
 module.exports = class Config {
   has (key) {
     const map = {
-      'fastify.host': 'localhost',
-      'fastify.port': 0
+      'revane.server.host': 'localhost',
+      'revane.server.port': 0
     }
     return map[key] != null
   }
 
   get (key) {
     const map = {
-      'fastify.host': 'localhost',
-      'fastify.port': 0
+      'revane.server.host': 'localhost',
+      'revane.server.port': 0
     }
     return map[key]
   }
