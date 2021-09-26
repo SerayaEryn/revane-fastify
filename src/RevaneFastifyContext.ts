425081a53066093ff3797631b1d13a2a11f0d9f2
Denis Fäcke <serayaeryn@gmail.com>
2021-09-26 10:22:24 +0200

    fix context types

--- src/RevaneFastifyContext.ts
+++ src/RevaneFastifyContext.ts
@@ -1,5 +1,5 @@
 export interface RevaneFastifyContext {
-  hasById (id: string): boolean
-  getByComponentType (type: string): any[]
-  getById (id: string): any
+  hasById (id: string): Promise<boolean>
+  getByComponentType (type: string): Promise<any[]>
+  getById (id: string): Promise<any>
 }
